4c4e966ac266ccfbb5290a87d4e558d0c7f2ff1d
Kenny Blanckaert <kenny.blanckaert@ebo-enterprises.com>
2022-12-25 19:48:20 +0100

    Resume - Programming Skills

---
+++ src/components/SkillSlider.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Container from '@mui/material/Container'
+import { makeStyles } from '@mui/styles';
+
+const useStyles = (props) => makeStyles({
+  skill: {
+    color: 'white',
+    margin: '0px !important',
+    maxWidth: '100%',
+    height: 50,
+    marginBottom: '3px !important',
+  },
+  skillIcon: {
+    maxWidth: '40px !important',
+    height: '40px !important',
+    marginTop: '0px !important',
+    marginLeft: '20px !important',
+    marginRight: '0px !important',
+    paddingTop: '10px !important',
+    textAlign: 'center',
+    border: '2px solid white',
+    borderRadius: 6,
+    fontFamily: 'Motor Oil 1937 M54',
+    fontSize: 12,
+    fontWeight: 'bold',
+    float: 'left',
+  },
+  skillName: {
+    width: '125px !important',
+    height: '40px !important',
+    margin: '0px !important',
+    float: 'left',
+    paddingTop: 10,
+    paddingLeft: 20,
+    fontFamily: 'Motor Oil 1937 M54',
+    fontWeight: 'bold',
+  },
+  skillSlider: {
+    color: 'white',
+    maxWidth: 300,
+    height: 20,
+    marginTop: 20,
+    marginLeft: '60px !important',
+    borderTop: '1.5px solid white',
+    float: 'left',
+  },
+  skillSliderMarker: {
+    maxWidth: 18,
+    height: 18,
+    paddingTop: '5px !important',
+    paddingBottom: '5px !important',
+    marginRight: '0px !important',
+    marginLeft: `calc(${props.percentage}% - 8px) !important`,
+    marginTop: -11,
+    border: '1px solid white',
+    borderRadius: 10
+  },
+  skillSliderMarkerDot: {
+    maxWidth: 6,
+    height: 6,
+    backgroundColor: 'white',
+    borderRadius: 20,
+  }
+});
+
+const SkillSLider = function({ name, abbreviation, percentage }) {
+  const props = {
+    percentage: percentage
+  }
+  const classes = useStyles(props)();
+
+  return (
+    <Container className={`${classes.skill}`} maxWidth={false} disableGutters>
+      <Container className={`${classes.skillIcon}`} maxWidth={false} disableGutters>{abbreviation}</Container>
+      <Container className={`${classes.skillName}`} maxWidth={false} disableGutters>{name}</Container>
+      <Container className={`${classes.skillSlider}`} maxWidth={false} disableGutters>
+        <Container className={`${classes.skillSliderMarker}`} maxWidth={false} disableGutters>
+          <Container className={`${classes.skillSliderMarkerDot}`} maxWidth={false} disableGutters></Container>
+        </Container>
+      </Container>
+    </Container>
+  );
+}
+
+export default SkillSLider;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,6 +4,8 @@ import { makeStyles } from '@mui/styles';
 
 import ResumeBackground from '../images/resume-background.jpg';
 
+import SkillSlider from '../components/SkillSlider';
+
 const useStyles = makeStyles({
   background: {
     backgroundImage: `url(${ResumeBackground})`,
@@ -15,7 +17,6 @@ const useStyles = makeStyles({
     width: '100%',
     margin: 0,
     padding: 0,
-    // overflow: 'scroll',
   },
   overlay: {
     backgroundColor: 'rgba(10, 10, 10, 0.5)',
@@ -30,16 +31,13 @@ const useStyles = makeStyles({
     float: 'left',
     marginTop: '70px !important',
     marginLeft: '30px !important',
+    maxWidth: '100%'
   },
   wide: {
     maxWidth: 650,
   },
   narrow: {
     maxWidth: 500,
-  },
-  section: {
-    border: '1px solid #0c3e52',
-    marginBottom: '20px'
   },
   third: {
     height: 270, // 270 x 3 + 2x margin (20) = 850
@@ -52,67 +50,101 @@ const useStyles = makeStyles({
   },
   threeFifths: {
     height: 498,
-  }
+  },
+  section: {
+    border: '1px solid #0c3e52',
+    marginBottom: '20px',
+    maxWidth: '100%'
+  },
+  sectionTitle: {
+    fontFamily: 'Motor Oil 1937 M54',
+    fontSize: 25,
+    color: '#ffffff',
+    marginTop: 20,
+    marginLeft: 20,
+    marginBottom: 50,
+    borderBottom: '3px solid #0c3e52',
+    display: 'inline-block',
+  },
 });
 
 const Resume = function() {
   const classes = useStyles();
 
+  const softwareSkills = [
+    { name: "Python", abbreviation: "Py", percentage: 90 },
+    { name: "Golang", abbreviation: "Go", percentage: 60 },
+    { name: "Javascript", abbreviation: "Js", percentage: 80 },
+    { name: "Java, C#, C++", abbreviation: "OO", percentage: 90 },
+    { name: "Databases", abbreviation: "SQL", percentage: 80 },
+  ]
+
   return (
-      <Container className={`${classes.background}`} maxWidth={false} disableGutters>
-        <Container className={`${classes.overlay}`} maxWidth={false} disableGutters>
-          <Container className={`${classes.wrapper}`} maxWidth={false} disableGutters>
+    <Container className={`${classes.background}`} maxWidth={false} disableGutters>
+      <Container className={`${classes.overlay}`} maxWidth={false} disableGutters>
+        <Container className={`${classes.wrapper}`} maxWidth={false} disableGutters>
 
-            <Container className={`${classes.column} ${classes.wide}`} maxWidth={false} disableGutters>
-              
-              {/* Programming Skills */}
-              <Container className={`${classes.section} ${classes.halve}`} maxWidth={false} disableGutters>
-
-              </Container>
-
-              {/* SecDevOps Skills */}
-              <Container className={`${classes.section} ${classes.halve}`} maxWidth={false} disableGutters>
-
-              </Container>
-
-            </Container>
-
-            <Container className={`${classes.column} ${classes.wide}`} maxWidth={false} disableGutters>
-
-              {/* Eduction */}
-              <Container className={`${classes.section} ${classes.threeFifths}`} maxWidth={false} disableGutters>
-
-              </Container>
-
-              {/* Experience */}
-              <Container className={`${classes.section} ${classes.twoFifths}`} maxWidth={false} disableGutters>
-
-              </Container>
-
-            </Container>
-
-            <Container className={`${classes.column} ${classes.narrow}`} maxWidth={false} disableGutters>
+          <Container className={`${classes.column} ${classes.wide}`} maxWidth={false} disableGutters>
             
-              {/* Languages */}
-              <Container className={`${classes.section} ${classes.third}`} maxWidth={false} disableGutters>
+            {/* Programming Skills */}
+            <Container className={`${classes.section} ${classes.halve}`} maxWidth={false} disableGutters>
+              <h2 className={`${classes.sectionTitle}`}>PROGRAMMING SKILLS</h2>
+              {
+                softwareSkills.map((skill) =>
+                  <SkillSlider 
+                    key={skill.name}
+                    name={skill.name} 
+                    abbreviation={skill.abbreviation} 
+                    percentage={skill.percentage}
+                  />
+                )
+              }
+            </Container>
 
-              </Container>
-
-              {/* Personal Skills */}
-              <Container className={`${classes.section} ${classes.third}`} maxWidth={false} disableGutters>
-
-              </Container>
-
-              {/* Hobbies & Interests */}
-              <Container className={`${classes.section} ${classes.third}`} maxWidth={false} disableGutters>
-
-              </Container>
+            {/* SecDevOps Skills */}
+            <Container className={`${classes.section} ${classes.halve}`} maxWidth={false} disableGutters>
+              <h2 className={`${classes.sectionTitle}`}>OPERATION SKILLS</h2>
 
             </Container>
 
           </Container>
+
+          <Container className={`${classes.column} ${classes.wide}`} maxWidth={false} disableGutters>
+
+            {/* Eduction */}
+            <Container className={`${classes.section} ${classes.threeFifths}`} maxWidth={false} disableGutters>
+
+            </Container>
+
+            {/* Experience */}
+            <Container className={`${classes.section} ${classes.twoFifths}`} maxWidth={false} disableGutters>
+
+            </Container>
+
+          </Container>
+
+          <Container className={`${classes.column} ${classes.narrow}`} maxWidth={false} disableGutters>
+          
+            {/* Languages */}
+            <Container className={`${classes.section} ${classes.third}`} maxWidth={false} disableGutters>
+
+            </Container>
+
+            {/* Personal Skills */}
+            <Container className={`${classes.section} ${classes.third}`} maxWidth={false} disableGutters>
+
+            </Container>
+
+            {/* Hobbies & Interests */}
+            <Container className={`${classes.section} ${classes.third}`} maxWidth={false} disableGutters>
+
+            </Container>
+
+          </Container>
+
         </Container>
       </Container>
+    </Container>
   );
 };
 
